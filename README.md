21b0a39edd13e2f8d9a7f9e043a9b4c277dae0f0
mkusner <matt.kusner@gmail.com>
2018-07-18 09:37:31 +0100

    cleaning code

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Counterfactual Fairness
 This repository has code for the paper:
 
-Matt J. Kusner\*, Joshua R. Loftus\*, Chris Russelli\*, Ricardo Silva [Counterfactual Fairness](https://arxiv.org/pdf/1703.06856.pdf) Neural Information Processing Systems (NIPS), 2017
+Matt J. Kusner\*, Joshua R. Loftus\*, Chris Russelli\*, Ricardo Silva [Counterfactual Fairness](https://arxiv.org/pdf/1703.06856.pdf) 
+Neural Information Processing Systems (NIPS), 2017
 \*=authors contributing equally
 
--- law_school_classifiers.R
+++ law_school_classifiers.R
@@ -1,6 +1,7 @@
 
 library(dplyr)
 library(caret)
+library(rstan)
 
 raw_data <- read.csv("law_data.csv")
 law <- dplyr::select(raw_data, race, sex, LSAT, UGPA, region_first, ZFYA, sander_index, first_pf) 
@@ -36,8 +37,8 @@ ne <- nrow(lawTest)
 lawTrain$LSAT <- round(lawTrain$LSAT)
 lawTest$LSAT <- round(lawTest$LSAT)
 
-# don't fit model transductively and don't use pass
-# -------------------------------------------------
+# don't fit model transductively
+# ------------------------------
 law_stan_train <- list(N = n, K = length(sense_cols), a = data.matrix(lawTrain[,sense_cols]), 
                           ugpa = lawTrain[,c("UGPA")], lsat = lawTrain[,c("LSAT")], zfya = lawTrain[,c("ZFYA")])
 
@@ -49,6 +50,8 @@ la_law_train <- extract(fit_law_train, permuted = TRUE)
 #u_te_samp <- colMeans(la_law_train$u_TE)
 U_TRAIN   <- colMeans(la_law_train$u)
 
+save(la_law_train,file='law_school_l_stan_train.Rdata')
+
 ugpa0      <- mean(la_law_train$ugpa0)
 eta_u_ugpa <- mean(la_law_train$eta_u_ugpa)
 eta_a_ugpa <- colMeans(la_law_train$eta_a_ugpa)
@@ -58,8 +61,6 @@ eta_u_lsat <- mean(la_law_train$eta_u_lsat)
 eta_a_lsat <- colMeans(la_law_train$eta_a_lsat)
 
 SIGMA_G <- mean(la_law_train$sigma_g)
-
-# get U_TEST by sampling from stan
 #----------------------------------
 law_stan_test <- list(N = ne, K = length(sense_cols), a = data.matrix(lawTest[,sense_cols]),
                       ugpa = lawTest[,c("UGPA")], lsat = lawTest[,c("LSAT")],
@@ -74,54 +75,7 @@ la_law_test <- extract(fit_law_test, permuted = TRUE)
 U_TEST   <- colMeans(la_law_test$u)
 
 
-output <- data.frame(bar_pass_fair = y,
-                     location = l,
-                     UGPA = g, 
-                     LSAT = t, 
-                     ZFYA = z,
-                     amerind = lawTrain$amerind,
-                     asian   = lawTrain$asian  ,
-                     black   = lawTrain$black  ,
-                     hisp    = lawTrain$hisp   ,
-                     mexican = lawTrain$mexican,
-                     other   = lawTrain$other  ,
-                     puerto  = lawTrain$puerto ,
-                     white   = lawTrain$white  ,
-                     female  = lawTrain$female,
-                     male    = lawTrain$male,
-                     u_hat = u_hat)
-
-output_te <- data.frame(bar_pass_fair = y_te,
-                        location = l_te,
-                        UGPA = g_te, 
-                        LSAT = t_te, 
-                        ZFYA = z_te,
-                        amerind = lawTest$amerind,
-                        asian   = lawTest$asian  ,
-                        black   = lawTest$black  ,
-                        hisp    = lawTest$hisp   ,
-                        mexican = lawTest$mexican,
-                        other   = lawTest$other  ,
-                        puerto  = lawTest$puerto ,
-                        white   = lawTest$white  ,
-                        female  = lawTest$female,
-                        male    = lawTest$male,
-                        u_hat = u_te_hat)
-
-write.csv(output, file = "law_school_l_stan_transductive_train.csv", row.names = TRUE)
-write.csv(output_te, file = "law_school_l_stan_transductive_test.csv", row.names = TRUE)
-
-save(la_law,file='law_school_l_stan_results.Rdata')
-
-
-
-
-
-
-
-
-
-
+save(la_law_test,file='law_school_l_stan_test.Rdata')
 
 
 # Classifiers on data
@@ -140,24 +94,22 @@ X_U_TE$UGPA <- lawTest$UGPA
 
 model_u <- lm(ZFYA ~ LSAT + UGPA + amerind + asian + black + hisp + mexican + other + puerto + white + male + female + 1, data=X_U)
 pred_u <- predict.glm(model_u)
-
-
 pred_u_te <- predict(model_u, newdata=X_U_TE)
-rmse_u_te <- sqrt( sum( (pred_u_te - X_U_TE$ZFYA)^2 ) / nrow(X_U_TE) )
-
+rmse_u_te <- sqrt( sum( (pred_u_te - X_U_TE$ZFYA)^2 ) / nrow(X_U_TE) ) # test error
+print('unfair full model:')
+print(rmse_u_te)
 
 
 # unaware (unfair)
 model_un <- lm(ZFYA ~ LSAT + UGPA + 1, data=X_U)
 pred_un <- predict.glm(model_un)
-
 pred_un_te <- predict(model_un, newdata=X_U_TE)
-rmse_un_te <- sqrt( sum( (pred_un_te - X_U_TE$ZFYA)^2 ) / nrow(X_U_TE) )
+rmse_un_te <- sqrt( sum( (pred_un_te - X_U_TE$ZFYA)^2 ) / nrow(X_U_TE) ) # test error
+print('unfair unaware model:')
+print(rmse_un_te)
 
 
-
-
-# fair
+# fair, non-deterministic model
 X_F <- data.frame(u=U_TRAIN, ZFYA=lawTrain$ZFYA)
 X_F_TE <- data.frame(u=U_TEST, ZFYA=lawTest$ZFYA)
 
@@ -165,10 +117,30 @@ model_f <- lm(ZFYA ~ u + 1, data=X_F)
 
 pred_f <- predict.glm(model_f)
 pred_f_te <- predict.glm(model_f, newdata=X_F_TE)
-
-rmse_f_te <- sqrt( sum( (pred_f_te - X_F_TE$ZFYA)^2 ) / nrow(X_F_TE) )
-
-
-
+rmse_f_te <- sqrt( sum( (pred_f_te - X_F_TE$ZFYA)^2 ) / nrow(X_F_TE) ) # test error
+print('fair non-deterministic model:')
+print(rmse_f_te)
 
 
+# fair, deterministic model
+model_ugpa <- lm(UGPA ~ amerind + asian + black + hisp + mexican + other + puerto + white + male + female + 1, data=lawTrain) # regress UGPA on race, sex
+model_lsat <- lm(LSAT ~ amerind + asian + black + hisp + mexican + other + puerto + white + male + female + 1, data=lawTrain) # regress LSAT on race, sex
+
+#resid
+lawTrain$resid_UGPA = lawTrain$UGPA - predict(model_ugpa, newdata=lawTrain)
+lawTrain$resid_LSAT = lawTrain$LSAT - predict(model_lsat, newdata=lawTrain)
+
+model_det <- lm(ZFYA ~ resid_UGPA + resid_LSAT + 1, data=lawTrain)
+
+model_ugpa_te <- lm(UGPA ~ amerind + asian + black + hisp + mexican + other + puerto + white + male + female + 1, data=lawTest)
+model_lsat_te <- lm(LSAT ~ amerind + asian + black + hisp + mexican + other + puerto + white + male + female + 1, data=lawTest)
+
+#resid
+lawTest$resid_UGPA = lawTest$UGPA - predict(model_ugpa_te, newdata=lawTest)
+lawTest$resid_LSAT = lawTest$LSAT - predict(model_lsat_te, newdata=lawTest)
+
+
+pred_det_te <- predict(model_det, newdata=lawTest)
+rmse_det_te <- sqrt( sum( (pred_det_te - lawTest$ZFYA)^2 ) / nrow(lawTest) ) # test error
+print('fair deterministic model:')
+print(rmse_det_te)
